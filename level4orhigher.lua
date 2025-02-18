5d152ed100383e2c137bee9286789f60cd5d806c
s4n8m2k6q1h9j7v5t3f2r9p7y2g8l3w6x7z9v5 <smilezrealscriptsalt@gmail.com>
2025-02-18 09:58:01 -0500

    Update and rename a.lua to level4orhigher.lua

--- level4orhigher.lua
+++ level4orhigher.lua
@@ -1,14 +1,4 @@
-local StarterGui = game:GetService("StarterGui")
-
-StarterGui:SetCore("SendNotification", {
-    Title = "S.E.T";
-    Text = "Loading...";
-    Duration = 5;
-    Button1 = "OK";
-    Button2 = "Cancel";
-    Callback = function(buttonPressed)
-        if buttonPressed == "OK" then
-            local function callback(Text)
+local function callback(Text)
 end
 
 local NotificationBindable = Instance.new("BindableFunction")
@@ -230,48 +220,3 @@ wait(5)
 print("--------------------------------------------------")
 print("S.E.T Checks Complete.")
 print("--------------------------------------------------")
-        elseif buttonPressed == "Cancel" then
-            game:GetService("StarterGui"):SetCore("DevConsoleVisible", true)
-                print("--------------------------------------------------")
-print("S.E.T UNC Test Starting...")
-print("--------------------------------------------------")
-wait(5)
-print("S.E.T UNC Test Running... Please Wait...")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SmilezReal/Executor-Tests/refs/heads/main/UncTest.lua",true))()
-
-print("--------------------------------------------------")
-print("S.E.T LVL Test Starting...")
-print("--------------------------------------------------")
-wait(5)
-print("S.E.T LVL Test Running... Please Wait...")
-
--- Try to run printidentity() but ignore errors
-local success, err = pcall(function()
-    printidentity()
-end)
-
-if not success then
-    warn("printidentity() failed: " .. err)
-end
-
-print("--------------------------------------------------")
-print("S.E.T VULN Test Starting...")
-print("--------------------------------------------------")
-wait(5)
-print("S.E.T VULN Test Running... Please Wait...")
-
--- Also protect the VULN test loadstring
-local success, err = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SmilezReal/Executor-Tests/refs/heads/main/VulnTest.lua", true))()
-end)
-if not success then
-    warn("VULN Test failed: " .. err)
-end
-
-print("--------------------------------------------------")
-print("S.E.T Checks Complete.")
-print("--------------------------------------------------")
-
-        end
-    end;
-})
